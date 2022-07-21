f940440d152d69f7e7a7fe22d2754e9fbc4aee8e
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-07-21 17:56:23 -0300

    final da primeira aula usando while

---
+++ main.js
@@ -0,0 +1,27 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template String
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+    
+}
+
+/*
+usar Crase eu consigo fazer template `#som_${variável}`
+Cara tem muito nisso.
+
+*/
