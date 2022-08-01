b7cdc224085562cc158cd794fb4c75e89991b0d6
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-01 18:44:39 -0300

    Foi adicionado um if para indentificação de Elemento

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio'){
+            elemento.play();
+    }
+    else{
+        console.log('Elemento não foi encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
