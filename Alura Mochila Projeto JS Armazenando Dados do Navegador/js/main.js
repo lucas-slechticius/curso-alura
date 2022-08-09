66c2304c0daf2c25d6fcaca505b77504d25b2d7e
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-09 12:32:09 -0300

    adicionando todos os itens do localStorage como um Array

--- Alura Mochila Projeto JS Armazenando Dados do Navegador/js/main.js
+++ Alura Mochila Projeto JS Armazenando Dados do Navegador/js/main.js
@@ -1,6 +1,10 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    console.log(elemento.nome, elemento.quantidade)
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
@@ -9,7 +13,7 @@ form.addEventListener("submit", (evento) => {
     const quantidade = evento.target.elements['quantidade']
 
     criaElemento(nome.value, quantidade.value)
-    
+
     nome.value = ""
     quantidade.value = ""
 })
@@ -20,8 +24,8 @@ function criaElemento(nome, quantidade) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = quantidade
-
     novoItem.appendChild(numeroItem)
+
     novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
@@ -33,5 +37,5 @@ function criaElemento(nome, quantidade) {
 
     itens.push(itemAtual)
 
-    localStorage.setItem("item", JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
