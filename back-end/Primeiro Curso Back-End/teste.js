70c398eb9a2714f8d1a06a2f77d9c4bc7e22d579
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-19 11:39:41 -0300

    Todos os testes númerais JS

--- back-end/Primeiro Curso Back-End/teste.js
+++ back-end/Primeiro Curso Back-End/teste.js
@@ -1,8 +1,27 @@
 const meuNumero = 3;
 
-const primeiroNumero = 1;
-const segundoNumero =2;
+const primeiroNumero = 45;
+const segundoNumero = 12;
 
 const operacaoMatematica = primeiroNumero + segundoNumero;
 
 console.log(operacaoMatematica);
+
+//.float famoso ponto flutante, já peguei sobre esses valores de variavel lá 
+// linguagem C, C++, vamos fazer uns testes.
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+//NaN => Not A Number (não é um número)
+
+const lifesound = "Lifesound";
+console.log(lifesound * primeiroNumero)
+
+// Isso vai retornar no painel um "NaN"
+
+
