e0d4dca371313fdf3e9e80a0aa5154ce34253a1b
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-22 10:41:26 -0300

    Scripts de teste

--- back-end/Primeiro Curso Back-End/boolean.js
+++ back-end/Primeiro Curso Back-End/boolean.js
@@ -1,6 +1,15 @@
 const primeiroNumero = 5;
 const segundoNumero = 10;
 
+let cadastroAtivado = true;
+
+const text = "teste"
+const text2 = "teste2"
+
+console.log(text === text2)
+
+console.log(text === text)
+
 console.log(primeiroNumero === segundoNumero);
 
 console.log(4 === 4);
---
+++ back-end/Primeiro Curso Back-End/script.js
@@ -0,0 +1,24 @@
+//var
+
+var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento
+
+console.log(area)
+
+// let
+
+let altura1 = 10
+let comprimento1 = 3
+
+
+let area1
+area1 = altura1 * comprimento1
+
+console.log(area1)
+
+
+let numeroDeCelular = 988888888
+
+console.log("seu numero é:", String(numeroDeCelular))
