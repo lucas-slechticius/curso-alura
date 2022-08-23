64cca0d9b9e35e3fdd79a40b5b9e960bfa2f463a
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-23 19:23:58 -0300

    funções básicas concluidas.

--- back-end/Primeiro Curso Back-End/funcoes.js
+++ back-end/Primeiro Curso Back-End/funcoes.js
@@ -4,6 +4,7 @@ let x = "";
     x = "oi";
     */
 
+// DECLARAÇÃO DE FUNÇÃO 
 
     //1) declarar a função
 
@@ -16,3 +17,18 @@ let x = "";
 
 imprimeTexto("oi")
 
+// da para colocar função dentro de função
+imprimeTexto(soma())
+
+//3) três formas de escrever funções 
+
+function soma() {
+ // o Return sempre deve (se existir na f) no final do bloco de código.
+ 
+    return 2 + 2
+}
+
+
+
+
+//console.log(soma())
