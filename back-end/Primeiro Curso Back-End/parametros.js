36e1ce697be5b0e029d5b3099d12519548a834d0
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-23 19:57:23 -0300

    Aqui coloquei funções chamando outras funções dentro do arquivo novo 'parametros.js'

---
+++ back-end/Primeiro Curso Back-End/parametros.js
@@ -0,0 +1,29 @@
+//parametros de função
+// function () <= !!! Isso ai, é o parametro, é
+// o que ela vai receber.
+
+
+function soma (num1, num2){
+    return num1 + num2
+}
+console.log(soma(2, 2))
+console.log(soma(10, 1))
+console.log(soma(10, 3))
+
+function nomeIdade(nome, idade){
+    
+    return `meu nome é ${nome} e minha idade é ${idade}`
+}
+
+console.log(nomeIdade("Lucas", 19))
+
+
+// function de multiplicação
+
+function multiplica(numero1, numero2){
+    return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4, 5), soma(3, 2)))
+
+
