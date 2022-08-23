81489a5e04cff678af4a717d5dd29485386ec18c
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-23 19:09:14 -0300

    funções de texto

--- back-end/Primeiro Curso Back-End/funcoes.js
+++ back-end/Primeiro Curso Back-End/funcoes.js
@@ -1,3 +1,18 @@
+/*
 let x = "";
-console.log(x);
-x = "oi";
+    console.log(x);
+    x = "oi";
+    */
+
+
+    //1) declarar a função
+
+                            //string
+    function imprimeTexto (texto) {
+        console.log(texto)
+    }
+
+//2) executa a função (1 ou + vezes)
+
+imprimeTexto("oi")
+
