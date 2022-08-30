68f6bbce3505813634b0d9d9eea4523eb22d7b08
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-08-30 11:33:12 -0300

    Arrow Functions!

---
+++ back-end/Primeiro Curso Back-End/expressoes.js
@@ -0,0 +1,13 @@
+const soma = function(num1, num2) {return num1 + num2}
+
+
+console.log(soma(1,1))
+
+
+console.log(apresentar())
+
+function apresentar(){
+    return "olá"
+}
+
+
--- back-end/Primeiro Curso Back-End/parametros.js
+++ back-end/Primeiro Curso Back-End/parametros.js
@@ -25,5 +25,3 @@ function multiplica(numero1 = 1, numero2 = 1){
 }
 
 console.log(multiplica(soma(4, 5), soma(3, 2)))
-
-
