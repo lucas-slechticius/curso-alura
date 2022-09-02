057e7180aaf4a618a94f2373f3a41ca7edffc5e9
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-02 18:31:41 -0300

    observações no media-for.js

--- back-end/Arrays/media-for.js
+++ back-end/Arrays/media-for.js
@@ -2,6 +2,11 @@ const notas = [10,  6.5, 8, 7.5]
 
 let somaDasNotas = 0
 
+// Sempre cuidar para não inserir uma váriavel dentro
+// do for, pois a váriavel pode se alterar e bagunçar o código
+// é uma boa prática sempre mander as váriaveis com um valor, fora
+// do for, para evitar esses bugs! =)
+
 for (let i = 0; i < notas.length; i++){
     somaDasNotas += notas[i];
 }
