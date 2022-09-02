bd2bb050f3fb973eafdb1c30452e8bcb69820b46
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-02 17:03:45 -0300

    Finalizado a Aula de Lista de Duas dimensões

--- back-end/Arrays/lista-duas-dim.js
+++ back-end/Arrays/lista-duas-dim.js
@@ -8,3 +8,19 @@ console.log(listaDeNotasEAlunos)
                     // primeiro é o indice, a ordem da lista, depois, é a ordem que
                     // inicia essa lista, ou seja [0] <= lista Alunos [0] <= ordem 0
 console.log(`${listaDeNotasEAlunos[0][0]}, sua media é ${listaDeNotasEAlunos[1][0]}`)
+
+
+const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayConcat = arrayOriginal.concat(["André", "Fernanda"], ["Ricardo", "Ana"], ["Marcelo", "Bia"])
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
+
+/*
+const arrayOriginal = [50, 60, 70]
+const arrayConcat = arrayOriginal.concat([80, [90, 100]])
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
+
+*/
