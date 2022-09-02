098de79b9aff46372d0803b5440db0de397a8176
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-02 16:41:26 -0300

    juntando-turmas.js tem exemplo do uso de .concat, concatenando arrays

---
+++ back-end/Arrays/juntando-turmas.js
@@ -0,0 +1,7 @@
+const salaDePhyton = ['Ricardo', 'Melissa', 'Helena']
+
+const salaDeJavaScript = ['Ju', 'Leo', 'Raquel']
+
+const salasUnificadas = salaDePhyton.concat(salaDeJavaScript)
+
+console.log(salasUnificadas)
