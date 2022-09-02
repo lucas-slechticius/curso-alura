00d3081ce6741e90d71319e44a4912c2231ca208
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-02 11:02:14 -0300

    Resultado da divisão

--- back-end/Arrays/dividindo-arrays.js
+++ back-end/Arrays/dividindo-arrays.js
@@ -3,5 +3,8 @@ const nomes = ['João', 'Juliana', 'Anna', 'Caio', 'Lara',
 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícios', 'Renan', 
 'Renata', 'Daisy', 'Camilo' ]
 
-const sala1 = nomes.slice(0,nomes.lenght/2)
+const sala1 = nomes.slice(0,nomes.length/2)
 const sala2 = nomes.slice(nomes.length/2)
+
+console.log(`Alunos da sala 1 são ${sala1}`)
+console.log(`Alunos da sala 2 são ${sala2}`)
