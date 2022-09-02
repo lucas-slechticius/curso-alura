6ffd0920970b90d0eb018bda90424e4ddb4b4e43
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-02 10:57:13 -0300

    Dividindo Arrays, primeiro exemplo

--- back-end/Arrays/dividindo-arrays.js
+++ back-end/Arrays/dividindo-arrays.js
@@ -1,2 +1,7 @@
-const nomes = ['João', 'João', 'João', 'João', 'João', 
-'pog' ]
+const nomes = ['João', 'Juliana', 'Anna', 'Caio', 'Lara', 
+'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre', 'Carlos',
+'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícios', 'Renan', 
+'Renata', 'Daisy', 'Camilo' ]
+
+const sala1 = nomes.slice(0,nomes.lenght/2)
+const sala2 = nomes.slice(nomes.length/2)
