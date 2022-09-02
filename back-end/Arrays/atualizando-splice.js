15a8c9304b8e5b06c85debdfabd0b9fbb4c03d01
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-02 11:20:14 -0300

    Exemplo de Splice

---
+++ back-end/Arrays/atualizando-splice.js
@@ -0,0 +1,14 @@
+const listaDeChamada = ['João', 'Juliana', 'Anna', 'Caio', 'Lara', 
+'Marjorie']
+
+listaDeChamada.splice(1,2, 'Rodrigo')
+
+console.log(`Lista de chamada: ${listaDeChamada}`)
+
+
+animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(1,0,'🐠')
+animaisDoAquario.splice(3,2,'🐟')
+
+console.log(animaisDoAquario)
