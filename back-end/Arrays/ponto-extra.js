93283f7282b11bd69da150e603c69520055a8a09
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-05 11:42:07 -0300

    uppercase.js - uma solução para deixar todas as strings de uma array em uppercase ou lowercase

--- back-end/Arrays/ponto-extra.js
+++ back-end/Arrays/ponto-extra.js
@@ -2,8 +2,11 @@ const notas = [10, 9, 8, 7, 6]
 
 const notasAtualizadas = notas.map( nota => nota == 10 ? nota : ++nota)
 
-// Eu escrevi "nota++", porém o JavaScript não carregou pois veio depois do valor
-// da váriavel, então o correto nessa situação, dentro de funções,
-// é sempre testar primeiro "++var" que funciona.
+// Eu escrevi "nota++", porém o JavaScript nessa situação, primeiro lê o valor
+// da váriavel, sendo 10, e depois adiciona uma unidade, totalizando 11, porém!
+// por causa disso, ele acaba "atualizando" o array, mas não mostra o valor
+// atualizado no console.log, só se eu por acaso colocar "nota++ , nota"
+// Por isso, sempre é importante nessa situação, fazer o JavaScript primeiro
+// adicionar o valor, que assim aparece, resultando em "++nota"
 
 console.log(notasAtualizadas)
---
+++ back-end/Arrays/uppercase.js
@@ -0,0 +1,6 @@
+let nomes = ["ana JUlia", "Caio vinicios", "BIA silva"]
+
+const nomesAtualizados = nomes.map(nome => nome.toUpperCase())
+// se eu quiser, eu posso deixar em min com = .toLowerCase
+
+console.log(nomesAtualizados)
