bd0ed9d672d668a06009f1f698c55e020462141f
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-05 10:42:33 -0300

    For Each Arrow e Function

--- back-end/Arrays/media-foreach.js
+++ back-end/Arrays/media-foreach.js
@@ -2,6 +2,13 @@ const notas = [10, 6.5, 8, 7.5]
 
 let somaDasNotas = 0
 
+/*
+    notas.forEach(function (nota){
+        somaDasNotas += nota
+    })
+*/
+
+
 notas.forEach( nota => {
     somaDasNotas += nota    
 });
