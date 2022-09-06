57f6eec8a52c1faad100917e735bc3e932b007cc
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-09-06 09:46:21 -0300

    Curso de Orientação de Objetos

---
+++ back-end/Orientação de Objetos/objetos.js
@@ -0,0 +1,23 @@
+const listaCPFs = ["1234563642", "14123123213", "9495325924"]
+
+
+/*
+    o OBjeto é a estrutura de Dados mais importante do JavaScript
+
+    Isso por que, um array, não consegue armazenar dados de forma ordenada e com
+    um sentido, seguindo nesse exemplo \/
+//const cliente = ["nome", "andre", "idade", "36"]
+
+    Como pode ser notado, não existe uma forma de eu mostrar, ou utilizar
+    em um array, o sentido de nome= andre, idade=36, para isso existem 
+    os objetos, são Variaveis com nome, e uma séria de "caracteristicas"
+    ou ID's, enfim, vou estudando sobre e relatando nesse curso! =)
+
+    */
+
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf:"123456789",
+    email: "andre@email.com"
+}
